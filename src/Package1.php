e3ee3e62923171a0679a7bf9a28d997bde2a5f3c
David mattei <d.mattei89@gmail.com>
2022-06-29 22:09:21 +0200

    chore: composer and test class

---
+++ src/Package1.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fabricity\Package1;
+
+final class Package1
+{
+    public static function demo(): string
+    {
+        return 'demo package1';
+    }
+}
